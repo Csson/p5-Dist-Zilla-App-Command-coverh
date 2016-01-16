8744ac472334d1e3266df2a656fe9be31f38450d
Erik Carlsson <info@code301.com>
2016-01-16 23:51:08 +0000

    Init

---
+++ lib/Dist/Zilla/App/Command/coverh.pm
@@ -0,0 +1,28 @@
+use 5.14.0;
+use strict;
+use warnings;
+
+package Dist::Zilla::App::Command::coverh;
+
+# VERSION
+# ABSTRACT: Short intro
+
+
+
+1;
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use Dist::Zilla::App::Command::coverh;
+
+=head1 DESCRIPTION
+
+Dist::Zilla::App::Command::coverh is ...
+
+=head1 SEE ALSO
+
+=cut
